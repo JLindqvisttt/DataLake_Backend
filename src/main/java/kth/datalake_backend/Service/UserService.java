0ce5cddd7725c8fae233d30fcc50d5ea514ef226
Nymy <nonno@kth.se>
2023-02-15 17:13:47 +0100

    Update UserService.java

--- src/main/java/kth/datalake_backend/Service/UserService.java
+++ src/main/java/kth/datalake_backend/Service/UserService.java
@@ -27,56 +27,54 @@ import java.util.stream.Collectors;
 @Service
 public class UserService {
 
-  @Autowired
-  AuthenticationManager authenticationManager;
-  @Autowired
-  UserRepository userRepository;
-  @Autowired
-  PasswordEncoder encoder;
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    PasswordEncoder encoder;
 
 
-  @Autowired
-  JwtUtils jwtUtils;
+    @Autowired
+    JwtUtils jwtUtils;
 
-  public ResponseEntity authenticateUser(String username, String password) {
-    Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-    SecurityContextHolder.getContext().setAuthentication(authentication);
-    String jwt = jwtUtils.generateJwtToken(username);
-    UserDetailsImp userDetails = (UserDetailsImp) authentication.getPrincipal();
+    public ResponseEntity authenticateUser(String username, String password) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(username);
+        UserDetailsImp userDetails = (UserDetailsImp) authentication.getPrincipal();
 
-    return ResponseEntity.ok(new JwtResponse(jwt, userDetails.getIdentity(), userDetails.getUsername(), userDetails.getFirstName(), userDetails.getLastName(), userDetails.getRoles().toString(), userDetails.getAvailableDatabases().stream().toList()));
-  }
-
-  public ResponseEntity<?> registerUser(SignUpRequest signUpRequest) {
-    if (userRepository.existsByUsername(signUpRequest.getUsername()))
-      return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already in use!"));
-
-    User user = new User(signUpRequest.getFirstname(), signUpRequest.getLastname(), signUpRequest.getUsername(), encoder.encode(signUpRequest.getPassword()), ERole.ROLE_USER);
-    userRepository.save(user);
-    return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
-  }
-
-  public List<User> getAllUser() {
-    return userRepository.findAll();
-  }
-
-  public ResponseEntity updateUser(UpdateUserRequest updateUserRequest) {
-    System.out.println("JONTE ÄR BÄST " + updateUserRequest.getIdentity());
-
-    if (userRepository.findByIdentity(updateUserRequest.getIdentity()) == null) {
-      System.out.println("SUCKS TO SUCK");
-      return ResponseEntity.badRequest().body("User not found");
+        return ResponseEntity.ok(new JwtResponse(jwt, userDetails.getIdentity(), userDetails.getUsername(), userDetails.getFirstName(), userDetails.getLastName(), userDetails.getRoles().toString(), userDetails.getAvailableDatabases().stream().toList()));
     }
-    User successUser = new User(updateUserRequest.getIdentity(),
-            updateUserRequest.getFirstname(),
-            updateUserRequest.getLastname(),
-            updateUserRequest.getUsername(),
-            encoder.encode(updateUserRequest.getPassword()),
-            updateUserRequest.getAvailableDatabases(),
-            updateUserRequest.getRole());
-    userRepository.save(successUser);
-    return ResponseEntity.ok(new MessageResponse("Successfully updated user"));
-  }
+
+    public ResponseEntity<?> registerUser(SignUpRequest signUpRequest) {
+        if (userRepository.existsByUsername(signUpRequest.getUsername()))
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already in use!"));
+
+        User user = new User(signUpRequest.getFirstname(), signUpRequest.getLastname(), signUpRequest.getUsername(), encoder.encode(signUpRequest.getPassword()), ERole.ROLE_USER);
+        userRepository.save(user);
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+
+    public List<User> getAllUser() {
+        return userRepository.findAll();
+    }
+
+    public ResponseEntity updateUser(UpdateUserRequest updateUserRequest) {
+
+        if (userRepository.findByIdentity(updateUserRequest.getIdentity()) == null)
+            return ResponseEntity.badRequest().body("User not found");
+
+        User successUser = new User(updateUserRequest.getIdentity(),
+                updateUserRequest.getFirstname(),
+                updateUserRequest.getLastname(),
+                updateUserRequest.getUsername(),
+                encoder.encode(updateUserRequest.getPassword()),
+                updateUserRequest.getAvailableDatabases(),
+                updateUserRequest.getRole());
+        userRepository.save(successUser);
+        return ResponseEntity.ok(new MessageResponse("Successfully updated user"));
+    }
 
 
 }
